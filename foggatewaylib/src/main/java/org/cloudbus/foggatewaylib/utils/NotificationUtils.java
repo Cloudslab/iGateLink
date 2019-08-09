28ffacbe68ff7ab780108a8e1a64037e2e344ebc
Riccardo Mancini <r.mancini@santannapisa.it>
2019-08-09 10:33:40 +1000

    Documentation added to NotificationUtils.

--- foggatewaylib/src/main/java/org/cloudbus/foggatewaylib/utils/NotificationUtils.java
+++ foggatewaylib/src/main/java/org/cloudbus/foggatewaylib/utils/NotificationUtils.java
@@ -11,6 +11,11 @@ import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 
+/**
+ * Utility functions for managing notifications.
+ *
+ * @author Riccardo Mancini
+ */
 public class NotificationUtils {
 
     public static final String DEFAULT_CHANNEL_ID = "default";
@@ -20,6 +25,19 @@ public class NotificationUtils {
     public static final String DEFAULT_NOTIFICATION_TITLE = "App is running";
     public static final String DEFAULT_NOTIFICATION_DESCRIPTION = "Service running in background";
 
+    /**
+     * Creates a {@link NotificationChannel} with the given {@code channel_id}, {@code name},
+     * {@code description} and {@code importance}.
+     * No action will be taken if {@link Build.VERSION#SDK_INT} is lower than 26.
+     *
+     * @param context the {@link Context} in which {@link Context#getSystemService(String)}
+     *                will be called.
+     * @param channel_id the id of the channel
+     * @param name the name of the channel
+     * @param description the description of the channel
+     * @param importance the importance of the channel
+     * @see NotificationChannel#NotificationChannel(String, CharSequence, int)
+     */
     public static void initChannel(Context context, String channel_id, String name,
                                    String description, int importance) {
         if (Build.VERSION.SDK_INT < 26) {
@@ -33,11 +51,37 @@ public class NotificationUtils {
         notificationManager.createNotificationChannel(channel);
     }
 
+    /**
+     * Creates a channel with default values.
+     *
+     * @param context the {@link Context} in which {@link Context#getSystemService(String)}
+     *                will be called.
+     * @see #DEFAULT_CHANNEL_ID
+     * @see #DEFAULT_CHANNEL_NAME
+     * @see #DEFAULT_CHANNEL_DESCRIPTION
+     * @see NotificationManager#IMPORTANCE_DEFAULT
+     */
     public static void initDefaultChannel(Context context) {
         initChannel(context, DEFAULT_CHANNEL_ID, DEFAULT_CHANNEL_NAME, DEFAULT_CHANNEL_DESCRIPTION,
                 NotificationManager.IMPORTANCE_DEFAULT);
     }
 
+    /**
+     * Builds a notification with the given parameters.
+     *
+     * @param context the context used to create the notification.
+     * @param intent the intent of the notification.
+     * @param title the title of the notification.
+     * @param text the text of the notification.
+     * @param icon the icon of the notification.
+     * @param channelId the id of the channel in which to publish the notification.
+     * @return the built {@link Notification}
+     *
+     * @see NotificationCompat.Builder#Builder(Context, String)
+     * @see NotificationCompat.Builder#setContentTitle(CharSequence)
+     * @see NotificationCompat.Builder#setContentText(CharSequence)
+     * @see NotificationCompat.Builder#setContentIntent(PendingIntent)
+     */
     public static Notification buildNotification(Context context, PendingIntent intent,
                                                  String title, String text, int icon,
                                                  String channelId){
@@ -53,6 +97,17 @@ public class NotificationUtils {
         return notificationBuilder.build();
     }
 
+    /**
+     * Builds a notification with default parameters.
+     *
+     * @param context the context used to create the notification.
+     * @param activity the class of the activity to start when the notification is clicked.
+     * @return the built {@link Notification}
+     * @see #buildNotification(Context, PendingIntent, String, String, int, String)
+     * @see #DEFAULT_NOTIFICATION_TITLE
+     * @see #DEFAULT_NOTIFICATION_DESCRIPTION
+     * @see #DEFAULT_CHANNEL_ID
+     */
     public static Notification buildDefaultNotification(Context context,
                                                         Class<? extends Activity> activity) {
         Intent notificationIntent = new Intent(context, activity);
