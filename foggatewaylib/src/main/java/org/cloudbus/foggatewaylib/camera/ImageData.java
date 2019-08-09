1d4b1268ea9378e60c4454e688fccf41305b414d
Riccardo Mancini <r.mancini@santannapisa.it>
2019-08-09 12:34:08 +1000

    Documentation added to ImageData.

--- foggatewaylib/src/main/java/org/cloudbus/foggatewaylib/camera/ImageData.java
+++ foggatewaylib/src/main/java/org/cloudbus/foggatewaylib/camera/ImageData.java
@@ -1,23 +1,48 @@
 package org.cloudbus.foggatewaylib.camera;
 
+import android.content.res.Configuration;
 import android.util.Log;
 
 import org.cloudbus.foggatewaylib.Data;
 
 import java.util.Date;
 
+/**
+ * Type of {@link Data} used for images taken by the camera, stored as a byte array with
+ * orientation information.
+ *
+ * @author Riccardo Mancini
+ */
 public class ImageData extends Data {
+    public static final int ORIENTATION_IGNORE = -1;
+
     private byte[] bytes;
+
+    /**
+     * Orientation of the image, should be one of:
+     * <ul>
+     *     <li>{@link #ORIENTATION_IGNORE}</li>
+     *     <li>{@link Configuration#ORIENTATION_UNDEFINED}</li>
+     *     <li>{@link Configuration#ORIENTATION_PORTRAIT}</li>
+     *     <li>{@link Configuration#ORIENTATION_LANDSCAPE}</li>
+     * </ul>
+     */
     private int orientation;
 
-
+    /**
+     * Creates an instance with given {@code bytes} and {@code orientation}, id set to
+     * {@code timestamp} in milliseconds and request id {@code 0}.
+     */
     public ImageData(Date timestamp, byte[] bytes, int orientation){
         super(timestamp);
         this.bytes = bytes;
         this.orientation = orientation;
     }
 
-
+    /**
+     * Creates an instance with given {@code bytes} and {@code orientation}, id set to the default
+     * auto-incrementing one and request id {@code 0}.
+     */
     public ImageData(byte[] bytes, int orientation){
         super();
         this.bytes = bytes;
@@ -25,8 +50,13 @@ public class ImageData extends Data {
         Log.d("DEBUG", "Orientation is " + orientation);
     }
 
+    /**
+     * Creates an instance with given {@code bytes}, {@code orientation} set to
+     * {@link #ORIENTATION_IGNORE}, id set to the default auto-incrementing one and request id
+     * {@code 0}.
+     */
     public ImageData(byte[] bytes){
-        this(bytes, -1);
+        this(bytes, ORIENTATION_IGNORE);
     }
 
     public byte[] getBytes() {
