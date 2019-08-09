2d60ff5c1954d0ca0c9589d609cb8c0da9ef3858
Riccardo Mancini <r.mancini@santannapisa.it>
2019-08-09 11:55:12 +1000

    Documentation added to Timer.

--- foggatewaylib/src/main/java/org/cloudbus/foggatewaylib/utils/Timer.java
+++ foggatewaylib/src/main/java/org/cloudbus/foggatewaylib/utils/Timer.java
@@ -1,22 +1,43 @@
 package org.cloudbus.foggatewaylib.utils;
 
-import android.os.Handler;
-
 import java.util.TimerTask;
 
+/**
+ * Simple timer implementation wrapping {@link java.util.Timer}.
+ *
+ * @see java.util.Timer
+ *
+ * @author Riccardo Mancini
+ */
 public class Timer {
     private java.util.Timer timer;
-    private Handler handler = new Handler();
     private int delay;
     private int period;
     private Runnable runnable;
 
+    /**
+     * Constructor that creates a timer that will run the given {@link Runnable} every
+     * {@code period} after an initial {@code delay}.
+     * This does not start the timer.
+     *
+     * @param runnable the code to be run.
+     * @param delay initial delay in milliseconds.
+     * @param period period in milliseconds
+     * @see #start()
+     * @see java.util.Timer#schedule(TimerTask, long, long)
+     */
     public Timer(Runnable runnable, int delay, int period){
         this.runnable = runnable;
         this.delay = delay;
         this.period = period;
     }
 
+    /**
+     * Stops the timer.
+     *
+     * @see java.util.Timer#cancel()
+     * @see java.util.Timer#purge()
+     */
     public void stop(){
         if(timer != null){
             timer.cancel();
@@ -24,11 +45,18 @@ public class Timer {
         }
     }
 
+    /**
+     * Starts the timer.
+     *
+     * @see java.util.Timer#Timer()
+     * @see TimerTask
+     * @see java.util.Timer#schedule(TimerTask, long, long)
+     */
     public void start(){
         timer = new java.util.Timer();
         TimerTask timerTask = new TimerTask() {
             public void run() {
-                handler.post(runnable);
+                runnable.run();
             }
         };
         timer.schedule(timerTask, delay, period);
