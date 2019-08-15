7f326846b5b0361124365197c30941ae7f76bd31
Riccardo Mancini <r.mancini@santannapisa.it>
2019-08-15 13:37:25 +1000

    Documented SimpleBluetoothLeAdapter.

--- bluetooth/src/main/java/org/cloudbus/foggatewaylib/bluetooth/SimpleBluetoothLeAdapter.java
+++ bluetooth/src/main/java/org/cloudbus/foggatewaylib/bluetooth/SimpleBluetoothLeAdapter.java
@@ -9,6 +9,7 @@ import android.os.Handler;
 
 /**
  * Simple Bluetooth Low Energy adapter.
+ * This class simplifies the operations of scan and check of connected devices.
  *
  * @see <a href="https://developer.android.com/guide/topics/connectivity/bluetooth-le#java">https://developer.android.com/guide/topics/connectivity/bluetooth-le#java</a>
  *
@@ -36,6 +37,10 @@ public class SimpleBluetoothLeAdapter {
 
     private OnScanStartStopListener mListener;
 
+    /**
+     * Constructs a new {@link SimpleBluetoothLeAdapter}, initializing the
+     * {@link BluetoothManager} and the {@link BluetoothAdapter} using the given {@link Context}.
+     */
     public SimpleBluetoothLeAdapter(Context context) {
         this.context = context;
 
@@ -43,10 +48,22 @@ public class SimpleBluetoothLeAdapter {
         bluetoothAdapter = bluetoothManager.getAdapter();
     }
 
+    /**
+     * Checks whether bluetooth is enabled.
+     *
+     * @return {@code true} if the bluetooth is enabled, {@code false} otherwise.
+     */
     public boolean checkBluetooth() {
         return bluetoothAdapter != null && bluetoothAdapter.isEnabled();
     }
 
+    /**
+     * Starts/stops a Bluetooth Low Energy scan.
+     * The scan will be automatically stopped after {@link #scanPeriod} milliseconds.
+     *
+     * @param enable {@code true} to start a scan, {@code false} to stop it.
+     * @see #setScanPeriod(int)
+     */
     private void scanLeDevice(final boolean enable) {
         if (leScanCallback == null)
             throw new RuntimeException("You have to set a scan callback using setLeScanCallback!");
@@ -70,40 +87,81 @@ public class SimpleBluetoothLeAdapter {
         }
     }
 
+    /**
+     * Starts a Bluetooth Low Energy scan.
+     * The scan will be automatically stopped after {@link #scanPeriod} milliseconds.
+     * @see #setScanPeriod(int)
+     */
     public void startScan(){
         scanLeDevice(true);
     }
 
+    /**
+     * Stops a Bluetooth Low Energy scan.
+     */
     public void stopScan(){
         scanLeDevice(false);
     }
 
+    /**
+     * Sets the duration of the scan in milliseconds.
+     */
     public void setScanPeriod(int scanPeriod) {
         this.scanPeriod = scanPeriod;
     }
 
+    /**
+     * Sets the callback to be called when new devices are found.
+     */
     public void setLeScanCallback(BluetoothAdapter.LeScanCallback leScanCallback) {
         this.leScanCallback = leScanCallback;
     }
 
+    /**
+     * Sets the callback to be called when the scan is started and stopped.
+     */
     public void setOnScanStartStopListener(OnScanStartStopListener listener){
         mListener = listener;
     }
 
+    /**
+     * Returns the duration of the scan in milliseconds.
+     */
     public int getScanPeriod() {
         return scanPeriod;
     }
 
+    /**
+     * Checks whether the given device is connected.
+     *
+     * @param device the device
+     * @return {@code true} if the {@code device} is connected, {@code false} otherwise.
+     */
     public boolean isDeviceConnected(BluetoothDevice device){
         return bluetoothManager.getConnectedDevices(BluetoothProfile.GATT).contains(device);
     }
 
+    /**
+     * Checks whether a scan is currently in progress.
+     *
+     * @return {@code true} if a scan is running, {@code false} otherwise.
+     */
     public boolean isScanning(){
         return mScanning;
     }
 
+    /**
+     * Interface for receiving callbacks when the scan is started and stopped.
+     */
     public interface OnScanStartStopListener{
+        /**
+         * Called when a scan is started.
+         */
         void onScanStart();
+
+        /**
+         * Called when a scan is stopped.
+         */
         void onScanStop();
     }
 }
