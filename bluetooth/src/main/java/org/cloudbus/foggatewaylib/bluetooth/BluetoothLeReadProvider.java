6705a744ca68d69f79c4e30f5c1904f99f693bea
Riccardo Mancini <r.mancini@santannapisa.it>
2019-08-15 13:23:19 +1000

    Documented BluetoothLeReadProvider.

--- bluetooth/src/main/java/org/cloudbus/foggatewaylib/bluetooth/BluetoothLeReadProvider.java
+++ bluetooth/src/main/java/org/cloudbus/foggatewaylib/bluetooth/BluetoothLeReadProvider.java
@@ -4,21 +4,31 @@ import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
 
+import androidx.annotation.CallSuper;
+
 import org.cloudbus.foggatewaylib.core.Data;
 import org.cloudbus.foggatewaylib.core.VoidData;
 
 import java.util.UUID;
 
+/**
+ * {@link BluetoothLeProvider} that reads from the given GATT server, service and characteristic
+ * when executed and calls {@link #readCharacteristic(BluetoothGatt, BluetoothGattCharacteristic)}
+ * when the value is retrieved.
+ *
+ * @param <T> the type of the output data
+ *
+ * @author Riccardo Mancini
+ */
+//TODO requestID is ignored
+//TODO not tested
 public abstract class BluetoothLeReadProvider<T extends Data>
         extends BluetoothLeProvider<VoidData, T> {
 
     private BluetoothGattCallback callback = new BluetoothReadProviderGattCallback();
 
     /**
-     * @param service
-     * @param characteristic
-     * @param inputType      type of the input data.
-     * @param outputType     type of the output data.
+     * Constructs a provider with the given parameters.
      */
     public BluetoothLeReadProvider(BluetoothGatt gatt, UUID service, UUID characteristic,
                                    Class<T> outputType) {
@@ -26,24 +36,43 @@ public abstract class BluetoothLeReadProvider<T extends Data>
     }
 
     /**
-     * @param service
-     * @param characteristic
-     * @param inputType      type of the input data.
-     * @param outputType     type of the output data.
+     * Constructs a provider with the given parameters that listens to all possible devices.
      */
     public BluetoothLeReadProvider(UUID service, UUID characteristic,
                                    Class<T> outputType) {
         super(null, service, characteristic, VoidData.class, outputType);
     }
 
+    /**
+     * Initiates a read.
+     */
     @Override
     public void execute(long requestID, VoidData... input) {
         getBluetoothLeHandler().readCharacteristic(getGatt(), getService(), getCharacteristic());
     }
 
+    /**
+     * Called when the characteristic value is read, provides the output to be published.
+     *
+     * @param gatt the GATT server in which the read occurred.
+     * @param characteristic the read characteristic.
+     * @see BluetoothGattCharacteristic#getValue()
+     * @see BluetoothGattCallback#onCharacteristicRead(BluetoothGatt, BluetoothGattCharacteristic, int)
+     */
     protected abstract T readCharacteristic(BluetoothGatt gatt,
                                          BluetoothGattCharacteristic characteristic);
 
+    /**
+     * Called when the characteristic value is read, gets the output to be published and publishes
+     * it, if the {@code status} is {@link BluetoothGatt#GATT_SUCCESS}.
+     *
+     * @param gatt the GATT server in which the read occurred.
+     * @param characteristic the read characteristic.
+     * @param status {@link BluetoothGatt#GATT_SUCCESS} if the read was successful, something else
+     *               otherwise.
+     * @see BluetoothGattCallback#onCharacteristicRead(BluetoothGatt, BluetoothGattCharacteristic, int)
+     */
+    @CallSuper
     protected void onRead(BluetoothGatt gatt,
                         BluetoothGattCharacteristic characteristic,
                         int status){
@@ -54,6 +83,10 @@ public abstract class BluetoothLeReadProvider<T extends Data>
         }
     }
 
+    /**
+     * Custom callback that calls {@link #onRead(BluetoothGatt, BluetoothGattCharacteristic, int)}
+     * at {@link BluetoothGattCallback#onCharacteristicRead(BluetoothGatt, BluetoothGattCharacteristic, int)}.
+     */
     private class BluetoothReadProviderGattCallback extends BluetoothGattCallback{
         @Override
         public void onCharacteristicRead(BluetoothGatt gatt,
@@ -62,12 +95,20 @@ public abstract class BluetoothLeReadProvider<T extends Data>
         }
     }
 
+    /**
+     * When attached to a {@link org.cloudbus.foggatewaylib.core.ExecutionManager}, adds the
+     * callback to the {@link BluetoothLeHandler}.
+     */
     @Override
     public void onAttach() {
         super.onAttach();
         getBluetoothLeHandler().addCallback(getService(), getCharacteristic(), false, callback);
     }
 
+    /**
+     * When detached from a {@link org.cloudbus.foggatewaylib.core.ExecutionManager}, removes the
+     * callback from the {@link BluetoothLeHandler}.
+     */
     @Override
     public void onDetach() {
         super.onDetach();
