6178d05608746ae944260d059451eefb2e591492
Riccardo Mancini <r.mancini@santannapisa.it>
2019-09-03 12:15:09 +1000

    Added documentation to WSDLBuilder.

--- aneka/src/main/java/org/cloudbus/foggatewaylib/aneka/WSDLBuilder.java
+++ aneka/src/main/java/org/cloudbus/foggatewaylib/aneka/WSDLBuilder.java
@@ -8,7 +8,27 @@ import com.manjrasoft.aneka.TaskItem;
 
 import org.cloudbus.foggatewaylib.utils.StringUtils;
 
+/**
+ * Utility class for generating the most common WSDL classes.
+ *
+ * @author Riccardo Mancini
+ */
 public class WSDLBuilder {
+
+    /**
+     * Builds an array of {@link File}s that have the same {@code storageBucketId} from an array of
+     * file names (or relative paths) that are mapped from {@code path} on the server to
+     * {@code virtualPath} in the execution environment.
+     * This is especially useful for multiple files in the same directory. Furthermore, it can
+     * save much boiler even if used with {@code path} and/or {@code virtualPath} set to
+     * {@code ""} and a list of paths as {@code filenames}.
+     *
+     * @param storageBucketId the ID of the storage bucket.
+     * @param path the base path of the files in the server.
+     * @param virtualPath the base path of the files in the execution environment.
+     * @param filenames the names (or relative paths) of the files.
+     * @return the generated {@link ArrayOfFile}.
+     */
     public static ArrayOfFile buildArrayOfFile(String storageBucketId, String path,
                                                String virtualPath, String[] filenames){
         ArrayOfFile arrayOfFile = new ArrayOfFile();
@@ -16,13 +36,24 @@ public class WSDLBuilder {
         for (int i = 0; i < filenames.length; i++){
             files[i] = new File();
             files[i].setStorageBucketId(storageBucketId);
-            files[i].setVirtualPath(virtualPath + filenames[i]);
-            files[i].setPath(path + filenames[i]);
+
+            if (virtualPath.length() > 0 && virtualPath.charAt(virtualPath.length()-1) == '/')
+                files[i].setVirtualPath(virtualPath + filenames[i]);
+            else
+                files[i].setVirtualPath(virtualPath + '/' + filenames[i]);
+
+            if (path.length() > 0 && path.charAt(path.length()-1) == '/')
+                files[i].setPath(path + filenames[i]);
+            else
+                files[i].setPath(path + '/' + filenames[i]);
         }
         arrayOfFile.setFile(files);
         return arrayOfFile;
     }
 
+    /**
+     * Merges multiple {@link ArrayOfFile} in a single {@link ArrayOfFile}.
+     */
     public static ArrayOfFile mergeArrayOfFiles(ArrayOfFile... arrays){
         int totalLength = 0;
 
@@ -44,18 +75,9 @@ public class WSDLBuilder {
         return mergedArray;
     }
 
-    public static ArrayOfFile buildArrayOfFile(String storageBucketId, String path,
-                                               String virtualPath){
-        ArrayOfFile arrayOfFile = new ArrayOfFile();
-        File[] files = new File[1];
-        files[0] = new File();
-        files[0].setStorageBucketId(storageBucketId);
-        files[0].setVirtualPath(virtualPath);
-        files[0].setPath(path);
-        arrayOfFile.setFile(files);
-        return arrayOfFile;
-    }
-
+    /**
+     * Builds a {@link File} instance with given attributes.
+     */
     public static File buildFile(String storageBucketId, String path, String virtualPath){
         File file = new File();
         file.setStorageBucketId(storageBucketId);
@@ -64,6 +86,24 @@ public class WSDLBuilder {
         return file;
     }
 
+    /**
+     * Builds an {@link ArrayOfFile} with a single {@link File} with the given
+     * {@code storageBucketId}, {@code path} and {@code virtualPath}.
+     */
+    public static ArrayOfFile buildArrayOfFile(String storageBucketId, String path,
+                                               String virtualPath){
+        ArrayOfFile arrayOfFile = new ArrayOfFile();
+        arrayOfFile.setFile(new File[]{buildFile(storageBucketId, path, virtualPath)});
+        return arrayOfFile;
+    }
+
+    /**
+     * Builds an {@link ExecuteTaskItem} from the given command and list of arguments.
+     *
+     * @param cmd the command to be executed.
+     * @param args the arguments to pass to the command.
+     * @return the generated {@link ExecuteTaskItem}.
+     */
     public static ExecuteTaskItem buildExecuteTaskItem(String cmd, String... args){
         String arguments = StringUtils.join(" ", args);
         ExecuteTaskItem executeTaskItem = new ExecuteTaskItem();
@@ -72,6 +112,9 @@ public class WSDLBuilder {
         return executeTaskItem;
     }
 
+    /**
+     * Builds an {@link ArrayOfTaskItem} from the given {@link TaskItem}s.
+     */
     public static ArrayOfTaskItem buildArrayOfTaskItem(TaskItem... taskItems){
         ArrayOfTaskItem arrayOfTaskItem = new ArrayOfTaskItem();
         arrayOfTaskItem.setTaskItem(taskItems);
